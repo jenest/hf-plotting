d747f558e75807dda7a89c55480a3359292503f1
Jenna Delozier <52164248+jenest@users.noreply.github.com>
2020-01-21 14:00:37 -0500

    Updated plot gen command
    Doesn't quite work, but getting closer

--- plot-hf.py
+++ plot-hf.py
@@ -9,41 +9,54 @@
 
 import threading
 import time
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from subprocess import Popen, PIPE
 
 #plots the data using matplotlib
 def make_plot():
-	threads = range(0,300,5) #xs for plot (in seconds)
+        threads = range(0,300,5) #xs for plot (in seconds)
 
-	#debugging prints
-	print ("threads: ", threads) #print values for x axis
-	print ("runtime: ", runtime) #print values for y axis
+        #debugging prints
+        print ("threads: ", threads) #print values for x axis
+        print ("runtime: ", runtime) #print values for y axis
 
-	#TO DO: matplot lib command to generate plot:
+        #generate plot using matplotlob and pandas:
+        x1 = pd.Series(threads, name="Threads")
+        x2 = pd.Series(runtime, name="Runtime")
 
+        data1 = pd.concat([x1, x2], axis=1)
+        g = sns.lmplot("Threads", "Runtime", data1,
+                scatter_kws={"marker": ".", "color": "navy", "alpha": 0.4 },
+                line_kws={"linewidth": 1, "color": "orange"},
+                height=8, aspect=1);
+        plt.plot(ls="--", c=".1")
+        plt.title('OpenMP Threads vs. Runtime', fontsize=20)
 
 #generate the plot of threads vs runtime
 def gather_data():
 
-	make_plot()
+        make_plot()
 
-	runHF = Popen(['nvprof --cpu-profiling on ./hf water.xyz'], stdout=PIPE) #running my script and recording output
+        runHF = Popen(['nvprof --cpu-profiling on ./hf water.xyz'], stdout=PIPE) #running my script and recording output
                                #re-directing stdout to record echo-ed
                                #output from the script
-	datapointX = runHF.communicate()[0] #subprocess returns a stdout, stderr tuple, not interested in stderr
+        datapointX = runHF.communicate()[0] #subprocess returns a stdout, stderr tuple, not interested in stderr
 
-	getThreads = Popen(['./script.sh'], stdout=PIPE) #running my script and recording output
+        getThreads = Popen(['./script.sh'], stdout=PIPE) #running my script and recording output
                                #re-directing stdout to record echo-ed
                                #output from the script
-	datapointY = getThreads.communicate()[0] #subprocess returns a stdout, stderr tuple, not interested in stderr
+        datapointY = getThreads.communicate()[0] #subprocess returns a stdout, stderr tuple, not interested in stderr
 
-	#debugging print
-	print (datapoint)
-	runtime.append(float(datapoint))
+        #debugging print
+        print (datapoint)
+        runtime.append(float(datapoint))
 
 
 #MAIN (exec starts here)
 
 #array to record data (y-vals for plot)
 runtime = []
+
 
